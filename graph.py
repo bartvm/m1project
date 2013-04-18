f4ea39433e413d761f7e0c520c0a79a1dee1a2d0
Bart van Merriënboer <bart.vanmerrienboer@gmail.com>
2013-04-18 16:57:32 +0200

    distance(), stations_on_lines(), replace strings with integers
    Give networkx integers instead of strings.
    Haversine distance function (distance())
    Function that returns the stations on a given set of lines
    (stations_on_lines())

--- graph.py
+++ graph.py
@@ -1,6 +1,31 @@
 import networkx as nx
 import csv
 import matplotlib.pyplot as plt
+import math
+
+def distance(origin, destination):
+	lon1, lat1 = origin
+	lon2, lat2 = destination
+	radius = 6371009 # m
+
+	lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
+
+	dlat = lat2 - lat1
+	dlon = lon2 - lon1
+	a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+	c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+	d = radius * c
+
+	return d
+
+def stations_on_lines(G, lines):
+	stations = []
+	for connection in G.edges():
+		for line in G[connection[0]][connection[1]]['lines']:
+			if int(line[0]) in lines:
+				stations.append(connection[0])
+				stations.append(connection[1])
+	return list(set(stations))
 
 def load(stations_file="data/stations.csv", lines_file="data/lines.csv", connections_file="data/connections.csv"):
 
@@ -26,10 +51,11 @@ def load(stations_file="data/stations.csv", lines_file="data/lines.csv", connect
 	paths = {}
 
 	for station in stations:
-		G.add_node(station[0], pos=(float(station[15]), float(station[14])), label=station[11])
+		G.add_node(int(station[0]), pos=(float(station[15]), float(station[14])), label=station[11])
 
 	for connection in connections:
-		G.add_edge(connection[0], connection[1])
+		connection = map(int, connection)
+		G.add_edge(connection[0], connection[1], weight=distance(G.node[connection[0]]['pos'], G.node[connection[1]]['pos']))
 		for line in lines:
 			if line[0] == connection[2]:
 				line_colour = line[2]
